3aee92dbd17d31f59e3e30c0fc2ebc8791562f9a
Victoria Sabbatino Wermelinger <victoria.wermelinger@gmail.com>
2024-08-21 08:30:00 -0300

    update

--- cursoemvideo-Python/MUNDO_03/moeda.py
+++ cursoemvideo-Python/MUNDO_03/moeda.py
@@ -1,12 +1,17 @@
 # moeda.py
-def aumentar(preco, taxa):
+
+def aumentar(preco=0, taxa=0):
     return preco + (preco * taxa / 100)
 
-def diminuir(preco, taxa):
+def diminuir(preco=0, taxa=0):
     return preco - (preco * taxa / 100)
 
-def dobro(preco):
+def dobro(preco=0):
     return preco * 2
 
-def metade(preco):
+def metade(preco=0):
     return preco / 2
+
+def moeda(preco=0, moeda='R$'):
+    return f'{moeda}{preco:.2f}'.replace('.', ',')
+
