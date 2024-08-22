504c875c0520932907b1b8bc56e36804380dd9c6
Victoria Sabbatino Wermelinger <victoria.wermelinger@gmail.com>
2024-08-22 15:38:06 -0300

    Update da aula com exercícios

--- cursoemvideo-Python/MUNDO_03/moeda.py
+++ cursoemvideo-Python/MUNDO_03/moeda.py
@@ -1,17 +1,28 @@
 # moeda.py
 
-def aumentar(preco=0, taxa=0):
-    return preco + (preco * taxa / 100)
+def aumentar(preco=0, taxa=0, formato = False):
+    """ -> Calcular o aumento de um determinado preço, retornando o resultado com ou sem formatação.
+    :param: preço: o preço que se quer reajustar
+    :param: taxa: qual é a porcentagem do aumento
+    :param: formato: quer a saída formatada ou não?
+    :return: o valor reajustado, com ou sem formato.
+    """
+    res = preco + (preco * taxa / 100)
+    return res if formato is False else moeda(res)
 
-def diminuir(preco=0, taxa=0):
-    return preco - (preco * taxa / 100)
+def diminuir(preco=0, taxa=0, formato = False):
+    res = preco - (preco * taxa / 100)
+    return res if formato is False else moeda(res)
 
-def dobro(preco=0):
-    return preco * 2
+def dobro(preco=0, formato = False):
+    res = preco * 2
+    return res if not formato else moeda(res)
 
-def metade(preco=0):
-    return preco / 2
+def metade(preco=0, formato = False):
+    res = preco / 2
+    return res if not formato else moeda(res)
 
 def moeda(preco=0, moeda='R$'):
     return f'{moeda}{preco:.2f}'.replace('.', ',')
+
 
