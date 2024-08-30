b041fb5019f60205ffc2744a84a3233cec4e871f
Victoria Sabbatino Wermelinger <victoria.wermelinger@gmail.com>
2024-08-30 16:46:25 -0300

    update

--- cursoemvideo-Python/MUNDO_03/moeda.py
+++ cursoemvideo-Python/MUNDO_03/moeda.py
@@ -25,13 +25,14 @@ def metade(preco=0, formato = False):
 def moeda(preco=0, moeda='R$'):
     return f'{moeda}{preco:.2f}'.replace('.', ',')
 
-def resumindo (preco=0, taxaa=10, taxa=5):
+def resumo(preco=0, taxaa=10, taxar=5):
     print('-'*30)
     print('Resumo do valor'.center(30))
     print('-'*30)
     print(f'Preço analisado: \t{moeda(preco)}')
-    print(f'Dobro do preço: \t{dobro(preco, True)}')
-    print(f'Metade do preço: \t{metade(preco, True)}')
-    print(f'{taxar}% de redução: \t\t{diminuir(preco, taxa, True)}')
-    print(f'{taxaa}% de aumento: \t{aumentar(preco, taxaa, True)}')
+    print(f'Dobro do preço: \t{dobro(preco,True)}')
+    print(f'Metade do preço: \t{metade(preco,True)}')
+    print(f'{taxaa}% de aumento: \t{aumentar(preco,taxaa, True)}')
+    print(f'{taxar}% de redução: \t\t{diminuir(preco, taxar, True)}')
     print('-' * 30)
+
